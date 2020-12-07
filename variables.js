52f5f2ed9ffc18750a47ecb29ff755ef7518d69f
AJ McDougall <anna.mcdougall@gmail.com>
2020-12-07 15:21:50 +0100

    What is a variable?

---
+++ variables.js
@@ -0,0 +1,23 @@
+// What is a variable?
+
+// A variable represents a piece of information
+// Variables are labels
+
+let greeting = "Hello";
+console.log(greeting);
+
+// Example 1: My mum
+// Different variables and constants can hold the same value
+
+var mum = "Virginia";
+const grandma = "Virginia"; // This is technically a constant and not a variable! Be careful!
+let marzipan = "Virginia";
+
+console.log(greeting + "Virginia");
+
+// Example 2: "The report"
+// A variable can change (vary!) depending on context or results
+
+var theReport = "10 page summary of the news today";
+theReport = "A legal case against the Prime Minister of Australia";
+theReport = "An environmental impact statement";
