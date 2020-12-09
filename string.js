dbe50f66c23bcf3207f37f36218d91096fcf31c1
AJ McDougall <anna.mcdougall@gmail.com>
2020-12-09 07:11:33 +0100

    Add string code

---
+++ string.js
@@ -0,0 +1,10 @@
+// A string represents a piece of text
+
+let greeting = "Hello fair traveller";
+let favNumber = "7";
+
+// 'info', "info", and `info` are identical to JavaScript
+
+let str1 = "dogs breakfast"; // If you try to add an apostrophe in "dog's", the string will break
+let str2 = "cup of tea";
+let str3 = `howdy doody`; // Backticks are next to the 1 on an English keyboard
